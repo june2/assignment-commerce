c0119107c9e2cb529886c34ff6635bae1e53e150
june2 <kimjy4536@hotmail.com>
2025-05-26 22:41:26 +0900

    feat: 브랜드별 상품 초기 데이터 추가
    - 9개 브랜드(A~I) 데이터 삽입
    - 각 브랜드별 8개 카테고리 상품 데이터 추가 (상의, 아우터, 바지, 스니커즈, 가방, 모자, 양말, 액세서리)
    - 서버 실행 시 자동으로 초기 데이터 로드되도록 설정

--- src/main/java/com/commerce/demo/Application.java
+++ src/main/java/com/commerce/demo/Application.java
@@ -2,9 +2,6 @@ package com.commerce.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class Application {
